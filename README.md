db2c5020413b79635c31e826245860be36c1a4e4
Frees0u1 <loskyer@gmail.com>
2017-12-28 23:14:09 +0800

    add heapsort

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 * KMP   // 经典的串匹配算法
 * Singleton //单例模式
 * BinarySearch  //经典二分算法
+* heapSort  // 堆排序(内含Floyd建堆算法)
---
+++ heapSort.cpp
@@ -0,0 +1,61 @@
+/* 
+*  批量建堆以及堆排序  Floyd算法 1964
+*/
+#include <vector>
+#include <iostream>
+using namespace std;
+
+void percolateDown(vector<int>& nums, int n, int i){ //将nums[i]进行下滤 此时堆的边界为n
+    int j;
+    while(true){
+        int lc = 2*i+1, rc = 2*i +2;
+        j = i; //j为下一个该下滤的节点
+        if(lc < n && rc < n){
+            int tmp = (nums[lc] > nums[rc]) ? lc : rc;
+            if(nums[tmp] > nums[i]) j = tmp;
+        }
+        else if(rc == n){ //仅左节点合法
+            if(nums[lc] > nums[i]) j = lc;
+        } //else ; 已经是叶节点
+
+        if(j == i) break; //无需下滤
+
+        swap(nums[i], nums[j]);
+        i = j;
+    }
+    return;
+}
+
+
+void buildHeap(vector<int>& nums){
+    int n = nums.size();
+    int lastInternal = (n - 2) >> 1; // 节点i的父节点为 (i-1)>>1, 最后一个内节点即为末节点的父亲
+    for(int i = lastInternal; i >= 0; i--){
+        percolateDown(nums, nums.size(), i); //对每一个节点进行下滤
+    }
+    return;
+}
+
+void heapSort(vector<int>& nums){
+    buildHeap(nums); //初始建堆
+
+    int hi = nums.size();
+    while(hi > 1){
+        swap(nums[0], nums[hi - 1]);
+        percolateDown(nums, --hi, 0);
+    }
+    return;
+}
+
+
+int main(){
+    vector<int> nums{16,4,10,14,7,9,3,2,8,1};
+    heapSort(nums);
+
+    for(int i = 0; i < nums.size(); i++){
+        cout << nums[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
